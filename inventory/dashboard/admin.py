1622b7848332b7d5c70243c057944b7c25955b9c
Robson Taye <ragnarrobie@gmail.com>
2025-10-18 12:08:50 +0300

    12

--- inventory/dashboard/admin.py
+++ inventory/dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import product
+from django.contrib.auth.models import Group
+admin.site.site_header = 'RobInventory Dashboard'
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','category','Quantity')
+    list_filter = ('category',)
 # Register your models here.
+admin.site.register(product, ProductAdmin)
+
--- inventory/dashboard/models.py
+++ inventory/dashboard/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
-# Create your models here.
+CATEGORY = (
+    ('Stationary','Stationary'),
+    ('Electronics','Electronics'),
+    ('Food','Food'),
+)
+class product(models.Model):
+    name = models.CharField(max_length = 100, null = True)
+    category = models.CharField(max_length = 20,null = True)
+    Quantity = models.PositiveBigIntegerField(null = True)
+    
+    def __str__(self):
+        return f'{self.name}-{self.Quantity}'
