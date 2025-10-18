ac8dd8cc2655c4986137f4464f3d2c1a880c2893
Robson Taye <ragnarrobie@gmail.com>
2025-10-18 12:29:44 +0300

    try

--- inventory/dashboard/models.py
+++ inventory/dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 CATEGORY = (
     ('Stationary','Stationary'),
     ('Electronics','Electronics'),
@@ -11,3 +12,10 @@ class product(models.Model):
     
     def __str__(self):
         return f'{self.name}-{self.Quantity}'
+class Order(models.Model):
+    product = models.ForeignKey(product, on_delete=models.CASCADE, null = True)
+    staff = models.ForeignKey(User, models.CASCADE, null =True)
+    order_quantity= models.PositiveBigIntegerField(null = True)
+    date = models.DateTimeField(auto_now_add= True)
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff.username}'
