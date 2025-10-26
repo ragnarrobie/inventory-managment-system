93a9581870f2d8253d9b18a51e765fb872911b06
Robson Taye <ragnarrobie@gmail.com>
2025-10-26 11:00:52 +0300

    made the make request button work

--- inventory/dashboard/forms.py
+++ inventory/dashboard/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from .models import product
+from .models import product,Order
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = product
         fields = ['name','category','Quantity']
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['product','order_quantity']
--- inventory/dashboard/views.py
+++ inventory/dashboard/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
-from .models import product
-from .forms import ProductForm
+from .models import product,Order
+from .forms import ProductForm,OrderForm
 from django.contrib.auth.models import User
 
 def superuser_required(view_func):
@@ -23,8 +23,23 @@ def superuser_required(view_func):
 @login_required(login_url='user-login')
 @superuser_required
 def index(request):
-    """Superuser dashboard."""
-    return render(request, 'dashboard/index.html')
+    orders = Order.objects.all()
+    if request.method=='POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.staff = request.user 
+            instance.save()
+            return redirect('dashboard-index')
+        else:
+            print("Form errors:", form.errors)
+    else:
+        form = OrderForm()
+    context= {
+        'orders': orders,
+        'form' : form,
+    }
+    return render(request, 'dashboard/index.html',context)
 
 
 @login_required(login_url='user-login')
@@ -33,8 +48,23 @@ def staff_admin(request):
     user = request.user
     if not (user.is_staff or user.is_superuser):
         return render(request, 'dashboard/access_denied.html')
-    return render(request, 'dashboard/staff_admin.html')
 
+    orders = Order.objects.all()
+    if request.method=='POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.staff = request.user
+            instance.save()
+            return redirect('dashboard-staff-admin')
+    else:
+        form = OrderForm()
+    
+    context = {
+        'orders': orders,
+        'form': form
+    }
+    return render(request, 'dashboard/staff_admin.html', context)
 
 @login_required(login_url='user-login')
 def staff(request):
@@ -46,14 +76,13 @@ def staff(request):
     if not (user.is_staff or user.is_superuser):
         return render(request, 'dashboard/access_denied.html')
     return render(request, 'dashboard/staff.html',context)
+
 @login_required(login_url='user-login')
 def staff_detail(request,pk):
     workers = User.objects.get(id=pk)
     context = {
         'worker' : workers,
     }
-
-    
     return render(request,'dashboard/staff-detail.html',context)
 
 @login_required(login_url='user-login')
@@ -75,12 +104,14 @@ def products(request):
         return render(request, 'dashboard/access_denied.html')
     return render(request, 'dashboard/product.html',context)
 @login_required(login_url='user-login')
+
 def product_delete(request,pk):
     item = product.objects.get(id=pk)
     if request.method== 'POST':
         item.delete()
         return redirect('dashboard-product')
     return render(request, 'dashboard/Delete.html')
+
 @login_required(login_url='user-login')
 def product_edit(request,pk):
     item = product.objects.get(id=pk)
@@ -100,7 +131,11 @@ def product_edit(request,pk):
 
 @login_required(login_url='user-login')
 def order(request):
+    orders = Order.objects.all()
+    context = {
+        'orders': orders,
+    }
     user = request.user
     if not user.is_superuser:
         return render(request, 'dashboard/access_denied.html')
-    return render(request, 'dashboard/order.html')
+    return render(request, 'dashboard/order.html',context)
